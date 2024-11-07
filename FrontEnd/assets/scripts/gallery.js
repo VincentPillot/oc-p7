c8ffdeb51ad0e0c444c8096c8e7c65cb4c638af6
Vincent Pillot <pillot.vincent@hotmail.com>
2024-11-07 14:32:03 +0100

    Ajout d'une fonction pour recuperer tous les projets via l'api & ajout de la fonction permettant de les creer et les affichers cote front

--- FrontEnd/assets/scripts/gallery.js
+++ FrontEnd/assets/scripts/gallery.js
@@ -1,4 +1,5 @@
 const galleryContenair = document.querySelector('.gallery-filters-contenair');
+const projectsContenair = document.querySelector('.gallery')
 
 
 //Récupérer toute les catégories
@@ -31,4 +32,40 @@ function createFilters(categories) {
     }
 }
 
+//Récupérer tous les projets
+async function getProjects() {
+    const url = "http://localhost:5678/api/works";
+    try {
+        const response = await fetch(url);
+        if(!response.ok) {
+            throw new Error ('Status de la réponse : ' + response.status);
+        }
+
+        const json = await response.json();
+        createProjects(json);
+        console.log(json);
+    } catch (error) {
+        console.error("Une erreur es survenue lors de la récupération des projets : " + error.message);
+    }
+}
+
+//Creation et affichage de tous les projets
+function createProjects(projects) {
+    for(let i=0; i < projects.length; i++) {
+        const figure = document.createElement("figure");  
+        figure.setAttribute('data-categorie', projects[i].category.name);
+
+        const img = document.createElement('img');
+        img.src = projects[i].imageUrl;
+
+        const figcaption = document.createElement('figcaption');
+        figcaption.innerText = projects[i].title;
+
+        projectsContenair.appendChild(figure);
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+    }
+}
+
 getCategories();
+getProjects();
