a92088e045d7f781516aa0b36a063e2dd99e5085
Vincent Pillot <pillot.vincent@hotmail.com>
2024-11-18 11:27:59 +0100

    Creation d'une fonction pour mieux gerer le changement des elements de la page lors d'un login/logout, ajout de cas sur la banniere du mode edition et des filtres du portfolio

--- FrontEnd/assets/scripts/app.js
+++ FrontEnd/assets/scripts/app.js
@@ -1,21 +1,44 @@
+const editorModeBanner = document.querySelector('.editor-mode-banner');
 const navBarLogBtn = document.querySelector('.navbar-log-btn > a');
+const galleryFilter = document.querySelector('.gallery-filters-contenair')
 
+function isUserLoggedHandler() {
+    if(sessionStorage.getItem("token")) { 
+        //Si l'user es connecter :
 
-if(sessionStorage.getItem("token")) { 
-    navBarLogBtn.innerText = "logout";
-    navBarLogBtn.href = "#";
+        //Ajout du listener pour la déconnexion
+        navBarLogBtn.addEventListener('click', (e) => {
+            if(sessionStorage.getItem("token")) {
+                e.preventDefault();
+                sessionStorage.removeItem("token");
+                sessionStorage.removeItem("userId");
 
-    navBarLogBtn.addEventListener('click', (e) => {
-        if(sessionStorage.getItem("token")) {
-            e.preventDefault();
-            sessionStorage.removeItem("token");
-            sessionStorage.removeItem("userId");
+                isUserLoggedHandler();
+            }
+        })    
 
-            navBarLogBtn.innerText = "login"; 
-            navBarLogBtn.setAttribute("href", "./pages/login.html");
-        }
-    })
-} else {
-    navBarLogBtn.innerText = "login"; 
-    navBarLogBtn.href = "./pages/login.html";
+        //Affichage de la bannière d'édition
+        editorModeBanner.style.display = "flex";
+
+        //Affichage & gestion du bouton de déconnexion
+        navBarLogBtn.innerText = "logout";
+        navBarLogBtn.href = "#";
+            
+        //Display none des filtres du portfolio
+        galleryFilter.style.display = "none"  
+    } else {
+        //Si l'user n'es pas connecter :
+
+        //On cache la banniere d'edition
+        editorModeBanner.style.display = "none";
+
+        //Bouton de login
+        navBarLogBtn.innerText = "login"; 
+        navBarLogBtn.href = "./pages/login.html";
+
+        //On affiche les filtres du portfolio
+        galleryFilter.style.display = "flex" 
+    } 
 }
+
+isUserLoggedHandler()
