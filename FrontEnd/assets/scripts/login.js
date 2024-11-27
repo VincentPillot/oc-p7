29b67261c9173d3d513b0bbfef91ea6081d81cce
Vincent Pillot <pillot.vincent@hotmail.com>
2024-11-27 03:36:15 +0100

    Reecriture du script pour correspondre à l'ajout du fichier api.js et l'import du call api pour se log

--- FrontEnd/assets/scripts/login.js
+++ FrontEnd/assets/scripts/login.js
@@ -1,40 +1,27 @@
+import { logingIn } from "./api.js";
 const loginForm = document.querySelector("form");
 const errorMsg = document.querySelector('.form-error-msg ');
 
-loginForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            email: e.target[0].value,
-            password: e.target[1].value
-        })
-    }).then(response => {
-        if(!response.ok) {
-            errorMsg.style.display = "block";
-            errorMsg.innerText = "Erreur dans l'indentifiant ou le mot de passe.";
-            console.log(response);
-            throw new Error("Erreur dans la réponse");
-        }
-        return response.json();
-    }).then(data => {
-        console.log("data :", data);
-        logIn(200, data);
-    }).catch(error => {
-        console.error("Erreur lors de la requête :", error);
-    });
-});
+loginForm.addEventListener("submit", (e) => {e.preventDefault(); logIn(e)})
 
 
-function logIn(status, data) {
-    if(status != 200) {
-        console.error(status + " : " + data);
-    } else {
-        sessionStorage.setItem("userId", data.userId);
-        sessionStorage.setItem("token", data.token);
+
+async function logIn(e) {
+    let userIds = JSON.stringify({
+        email: e.target[0].value,
+        password: e.target[1].value
+    })
+
+    let loginInfos = await logingIn(userIds);
+
+    if(loginInfos.success) {
+        localStorage.setItem("userId", loginInfos.data.userId);
+        localStorage.setItem("token", loginInfos.data.token);
         window.location.replace("../index.html");
     }
+    else {
+        errorMsg.innerText = "Erreur dans l’identifiant ou le mot de passe";
+        errorMsg.style.display = "block";
+    }
+    console.log(loginInfos.data)
 }
